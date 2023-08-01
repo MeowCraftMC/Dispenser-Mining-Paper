1c107a182cad2a2e4e85abd8252a5b91f2eb5df6
LS-KR <Elihuso@outlook.com>
2023-08-01 17:33:16 +0800

    [+] Plant crops by Dispenser

--- src/main/java/io/github/elihuso/dispenseminingpaper/DispenserMiningPaper.java
+++ src/main/java/io/github/elihuso/dispenseminingpaper/DispenserMiningPaper.java
@@ -2,19 +2,21 @@ package io.github.elihuso.dispenseminingpaper;
 
 import io.github.elihuso.dispenseminingpaper.listener.BlockBreakListener;
 import io.github.elihuso.dispenseminingpaper.listener.BlockPlaceListener;
+import io.github.elihuso.dispenseminingpaper.listener.BlockPlantListener;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 
-public final class DispenserMiningPaper extends JavaPlugin implements Listener {
+public final class DispenserMiningPaper extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getPluginManager().registerEvents(new BlockBreakListener(this), this);
         Bukkit.getPluginManager().registerEvents(new BlockPlaceListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new BlockPlantListener(this), this);
         getLogger().log(Level.FINE, "Dispenser Mining Plugin Enabled");
     }
 
---
+++ src/main/java/io/github/elihuso/dispenseminingpaper/listener/BlockPlantListener.java
@@ -0,0 +1,84 @@
+package io.github.elihuso.dispenseminingpaper.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Dispenser;
+import org.bukkit.block.data.Directional;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+public class BlockPlantListener implements Listener {
+    private final Plugin plugin;
+    public BlockPlantListener(Plugin plugin) {this.plugin = plugin;};
+    static Material[][] plants = {
+            {Material.WHEAT_SEEDS, Material.WHEAT},
+            {Material.BEETROOT_SEEDS, Material.BEETROOTS},
+            {Material.CARROT,Material.CARROTS},
+            {Material.POTATO, Material.POTATOES},
+            {Material.PUMPKIN_SEEDS, Material.PUMPKIN_STEM},
+            {Material.MELON_SEEDS, Material.MELON_STEM}
+    };
+    @EventHandler
+    public void OnPlant(BlockDispenseEvent event) {
+        if (event.isCancelled())
+            return;
+        if (!event.getBlock().getType().equals(Material.DISPENSER))
+            return;
+
+        Block dispenserBlock = event.getBlock();
+        ItemStack item = event.getItem();
+        boolean isselected = false;
+
+        for(Material[] v : plants) {
+            if (v[0].equals(item.getType())) {
+                isselected = true;
+                break;
+            }
+        }
+        if (!isselected)
+            return;
+
+        Block target = dispenserBlock.getRelative(((Directional)dispenserBlock.getBlockData()).getFacing());
+        Block base = target.getRelative(0, -1, 0);
+
+        if (!target.getType().isAir())
+            return;
+        if (!base.getType().equals(Material.FARMLAND))
+            return;
+
+        event.setCancelled(true);
+
+        isselected = false;
+        for(Material[] v : plants) {
+            if (v[0].equals(item.getType())) {
+                target.setType(v[1]);
+                target.setBlockData(v[1].createBlockData());
+                target.getState().update();
+                isselected = true;
+            }
+        }
+
+        if (!isselected)
+            return;
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, ()->{
+            Dispenser dispenser = (Dispenser)(dispenserBlock.getState());
+            Inventory inventory = dispenser.getInventory();
+
+            for (int i = 0; i < inventory.getSize(); ++i) {
+                ItemStack invItem = inventory.getItem(i);
+                if (invItem.isSimilar(event.getItem())) {
+                    invItem.setAmount(invItem.getAmount() - 1);
+                    inventory.setItem(i, invItem);
+                    break;
+                }
+            }
+        }, 1);
+    }
+}
