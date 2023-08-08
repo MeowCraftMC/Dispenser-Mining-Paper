dc169c1824252b3d5b741d75a6dfc3fc8abc13a3
LS-KR <Elihuso@outlook.com>
2023-08-08 16:22:04 +0800

    [F] Fix sweet berries can only plant on the framland

--- src/main/java/io/github/elihuso/dispenseminingpaper/listener/BlockPlantListener.java
+++ src/main/java/io/github/elihuso/dispenseminingpaper/listener/BlockPlantListener.java
@@ -5,7 +5,6 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Dispenser;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.type.*;
 import org.bukkit.event.EventHandler;
@@ -13,21 +12,23 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.material.CocoaPlant;
 import org.bukkit.plugin.Plugin;
 
 public class BlockPlantListener implements Listener {
     private final Plugin plugin;
-    public BlockPlantListener(Plugin plugin) {this.plugin = plugin;};
+    public BlockPlantListener(Plugin plugin) {this.plugin = plugin;}
     static Material[][] plants = {
             {Material.WHEAT_SEEDS, Material.WHEAT},
             {Material.BEETROOT_SEEDS, Material.BEETROOTS},
             {Material.CARROT,Material.CARROTS},
             {Material.POTATO, Material.POTATOES},
             {Material.PUMPKIN_SEEDS, Material.PUMPKIN_STEM},
-            {Material.MELON_SEEDS, Material.MELON_STEM},
-            {Material.SWEET_BERRIES, Material.SWEET_BERRY_BUSH}
+            {Material.MELON_SEEDS, Material.MELON_STEM}
     };
+    Material[] Dirts = {
+            Material.GRASS_BLOCK, Material.DIRT, Material.COARSE_DIRT, Material.MYCELIUM, Material.PODZOL, Material.FARMLAND
+    };
+
     @EventHandler
     public void OnPlant(BlockDispenseEvent event) {
         if (event.isCancelled())
@@ -77,7 +78,7 @@ public class BlockPlantListener implements Listener {
 
             for (int i = 0; i < inventory.getSize(); ++i) {
                 ItemStack invItem = inventory.getItem(i);
-                if (invItem.isSimilar(event.getItem())) {
+                if (item.isSimilar(invItem) && (invItem != null)) {
                     invItem.setAmount(invItem.getAmount() - 1);
                     inventory.setItem(i, invItem);
                     break;
@@ -120,7 +121,53 @@ public class BlockPlantListener implements Listener {
 
             for (int i = 0; i < inventory.getSize(); ++i) {
                 ItemStack invItem = inventory.getItem(i);
-                if (invItem.isSimilar(event.getItem())) {
+                if (item.isSimilar(invItem) && (invItem != null)) {
+                    invItem.setAmount(invItem.getAmount() - 1);
+                    inventory.setItem(i, invItem);
+                    break;
+                }
+            }
+        }, 1);
+    }
+
+    @EventHandler
+    public void OnPlantSweetBerry(BlockDispenseEvent event) {
+        if (event.isCancelled())
+            return;
+        if (!event.getBlock().getType().equals(Material.DISPENSER))
+            return;
+
+        Block dispenserBlock = event.getBlock();
+        ItemStack item = event.getItem();
+
+        BlockFace facing = ((Directional)dispenserBlock.getBlockData()).getFacing();
+        Block target = dispenserBlock.getRelative(facing);
+        Block base = target.getRelative(0, -1, 0);
+
+        if (!item.getType().equals(Material.SWEET_BERRIES))
+            return;
+        if (!target.getType().isAir())
+            return;
+        for (Material v : Dirts) {
+            if (v.equals(base.getType())) {
+                event.setCancelled(true);
+                break;
+            }
+        }
+
+        if (!event.isCancelled())
+            return;
+        target.setType(Material.SWEET_BERRY_BUSH);
+        target.setBlockData(Material.SWEET_BERRY_BUSH.createBlockData());
+        target.getState().update();
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+            Dispenser dispenser = (Dispenser)(dispenserBlock.getState());
+            Inventory inventory = dispenser.getInventory();
+
+            for (int i = 0; i < inventory.getSize(); ++i) {
+                ItemStack invItem = inventory.getItem(i);
+                if (item.isSimilar(invItem) && (invItem != null)) {
                     invItem.setAmount(invItem.getAmount() - 1);
                     inventory.setItem(i, invItem);
                     break;
