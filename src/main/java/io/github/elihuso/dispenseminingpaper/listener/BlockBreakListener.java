2d7cc115ff91d5ad45375951f451d54216a56d97
Elihuso Quigley <Elihuso@outlook.com>
2023-09-05 08:55:05 +0800

    [+] Break Bedrock when dispense enchanted golden apple

--- src/main/java/io/github/elihuso/dispenseminingpaper/listener/BlockBreakListener.java
+++ src/main/java/io/github/elihuso/dispenseminingpaper/listener/BlockBreakListener.java
@@ -6,14 +6,11 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Dispenser;
 import org.bukkit.block.data.Directional;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 
 public class BlockBreakListener implements Listener {
@@ -62,5 +59,40 @@ public class BlockBreakListener implements Listener {
                 }
             }, 1);
         }
+    }
+
+    @EventHandler
+    public void onDispenseEnchantedGoldenApple(BlockDispenseEvent event) {
+        if (!event.getBlock().getType().equals(Material.DISPENSER)) {
+            return;
+        }
+
+        if (event.isCancelled()) {
+            return;
+        }
+
+        Block dispenserBlock = event.getBlock();
+        ItemStack item = event.getItem();
+
+        if (!item.getType().equals(Material.ENCHANTED_GOLDEN_APPLE))
+            return;
+
+        Block target = dispenserBlock.getRelative(((Directional) dispenserBlock.getBlockData()).getFacing());
+        if (!target.getType().equals(Material.BEDROCK))
+            return;
+        target.setType(Material.AIR);//Break Bedrock but no drop.
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+            Dispenser dispenser = (Dispenser) (dispenserBlock.getState());
+            Inventory inventory = dispenser.getInventory();
+
+            for (int i = 0; i < inventory.getSize(); ++i) {
+                if (item.equals(inventory.getItem(i))) {
+                    ItemStack invItem = inventory.getItem(i);
+                    invItem.setAmount(invItem.getAmount() - 1);
+                    inventory.setItem(i, invItem);
+                }
+            }
+        }, 1);
     }
 }
